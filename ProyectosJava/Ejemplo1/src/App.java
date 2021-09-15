7617f2631dd92aaa69523d73ca210e27dba4da1d
airebasi <90109726+airebasi@users.noreply.github.com>
2021-09-15 16:06:41 -0500

    Update App.java

--- ProyectosJava/Ejemplo1/src/App.java
+++ ProyectosJava/Ejemplo1/src/App.java
@@ -3,10 +3,11 @@ package Ejemplo1.src;
 public class App {
     public static void main(String[] args) throws Exception {
         int a = 1, b = 2, c = 10, suma = 0, i = 0;
-        do {
+        while (i != c);
+        {
             suma += a + b;
             System.out.println(suma);
             i++;
-        } while (i != c);
+        } 
     }
 }
