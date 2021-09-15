72d74d0353cfdcceaca97ac5c7ab51f671ef41d4
airebasi <90109726+airebasi@users.noreply.github.com>
2021-09-15 16:14:49 -0500

    Update App.java

--- ProyectosJava/Ejemplo1/src/App.java
+++ ProyectosJava/Ejemplo1/src/App.java
@@ -3,11 +3,13 @@ package Ejemplo1.src;
 public class App {
     public static void main(String[] args) throws Exception {
         int a = 1, b = 2, c = 10, suma = 0, i = 0;
-        while (i != c);
+        while (i != c)
         {
             suma += a + b;
             System.out.println(suma);
             i++;
         } 
+        // Profe, lo hice con do while y si jalo, tambien en C# y si jalo, aqui no se
+        // porque pero en teoria deberia funcionar, gracias de antemano
     }
 }
